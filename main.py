b8a7b69204f346c3a7188dcfb306b8e144fbb1d7
Jack Champagne <jack@aircycler.com>
2020-08-28 16:09:15 -0400

    Rewrite for youtube-dl

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from pytube import YouTube
 import shelve
 import pprint
 import requests
 import re
+import youtube_dl
+import threading
+import os
 
-# Define add channels method
+
+def download(video_url):
+    with youtube_dl.YoutubeDL({'quiet': True}) as ydl:
+        length = ydl.extract_info(video_url).get('duration')
+    lock.acquire()
+    global videos_downloaded
+    videos_downloaded += 1
+    global today_watchtime
+    today_watchtime += length
+    recent_videos.append(video_url)
+    print('Finished downloading ' + str(videos_downloaded))
+    lock.release()
+
+
 def new_channels(chans):
     print('Please enter youtube channel url you would like to add or press enter to stop')
     response = input()
@@ -17,97 +32,139 @@ def new_channels(chans):
 
         chans.append(response)
         response = input()
-    
+
     print('Channel(s) added successfully')
     return chans
-
-## END FUNCTIONS --BEGIN BODY
-
-# Load current list of youtubers
-with shelve.open('my-content') as db:
-    channels = db.get('channels')
-    if channels == None:
-        channels = []
-    recent_videos = db.get('recent')
-    if recent_videos == None:
-        recent_videos = []
-
-# Ask for any youtuber recommendations to add to download list
-    print('Would you like to add any youtubers to the watchlist? (y/n)')
-
-    valid_input = False
-    while not valid_input:
-        response = input()
-        if response in ['y', 'Y']:
-            valid_input = True
-            new_channels(channels)
-            continue
-        if response in ['n', 'N']:
-            valid_input = True
-            continue
-        else:
-            valid_input = False
-            print('Please enter a valid key.')
-
-# Save new youtubers to shelve
-    db['channels'] = channels
+# Fix this method so that it is clearer
 
 
-# Check each youtuber for newest video 
+def refresh_catalogue():
+    videos_to_download = check_for_new_videos()
+
+    # Multi-thread downloads videos
+    threads = []
+    for video in videos_to_download:
+        print('Generating ' + video + ' thread')
+        threads.append(threading.Thread(target=download, args=(video,)))
+    print('Starting dl threads')
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+    print('Killed dl threads')
+    
+
+# Returns list of youtubers with new videos
+def check_for_new_videos():
+    videos_to_download = []
     print('Ok, checking for new videos for:')
-    pprint.pprint(channels)  
-
+    pprint.pprint(channels)
     url_regex = re.compile(r'("commandMetadata":{"webCommandMetadata":{"url":"(\/watch\?v=.+?)"{1})')
-
-    local_watchtime = 0
-    videos_downloaded = 0
     for channel in channels:
-
-        page = requests.get(channel + '/videos') # Get current list of videos
+        page = requests.get(channel + '/videos')  # Get current list of videos
         page.raise_for_status()
         matched_vid = url_regex.search(page.text)
         new_vid_url = "https://www.youtube.com" + matched_vid.group(2)
-
         # Check to see if video has not been downloaded
+        global recent_videos
         if new_vid_url not in recent_videos:
-            recent_videos.append(new_vid_url)
+            videos_to_download.append(new_vid_url)
         else:
             print('No new videos from: ' + channel)
-            continue
-    
-        # Download video
-        yt = YouTube(new_vid_url)
-        print('Downloading: ' + yt.title + '\nFrom: ' + yt.author)
-        stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    print('Downloading videos from these channels:')
+    pprint.pprint(videos_to_download)
+    return videos_to_download
 
-        stream.download('./downloads/')
-        print('Download successful.')
 
-        print('Added ' + str(yt.length) + ' seconds to downloaded videos today')
-        local_watchtime += yt.length
-        videos_downloaded += 1
+def get_watchtime(watchtime_s):
+    seconds = watchtime_s % 60
+    watchtime_s //= 60
+    minutes = watchtime_s % 60
+    watchtime_s //= 60
+    hours = watchtime_s % 24
+    watchtime_s //= 24
+    days = watchtime_s
+    return str(days) + ' days ' + str(hours) + ':' + str(minutes) + ':' + str(seconds)
 
-    # Print total watch time
-    print('Done downloading ' + str(videos_downloaded) + ' video(s).')
-    total_watchtime = db.get('watchtime')
-    if total_watchtime == None:
-        total_watchtime = 0
-    total_watchtime += local_watchtime
 
-    # Get minutes and seconds for display
-    mins = total_watchtime // 60
-    seconds = total_watchtime % 60
-    sec_str = ''
-    if seconds < 10:
-        sec_str = '0' + str(seconds)
-    else:
-        sec_str = str(seconds)
-    print('Total video watchtime: ' + str(mins) + ":" + sec_str)
+def main():
+    # Load database info
+    ytdb = shelve.open('my-content')
+    global channels
+    channels = ytdb.get('channels')
+    if channels is None:
+        channels = []
+    global recent_videos
+    recent_videos = ytdb.get('recent')
+    if recent_videos is None:
+        recent_videos = []
+    pprint.pprint(recent_videos)
+    global watchtime
+    watchtime = ytdb.get('watchtime')
+    if watchtime is None:
+        watchtime = 0
 
-    # Save shelve
-    db['watchtime'] = total_watchtime
-    db['channels'] = channels
-    db['recent'] = recent_videos
+    # Print current channels
+    print('List of current channels:', end='')
+    pprint.pprint(channels)
 
-    print('Video profile saved. Goodbye')
+    # Ask for input
+    valid_input = False
+    while not valid_input:
+        print(r'''Please select an option:
+    [A]dd youtubers
+    [D]ownload recent videos
+    [R]emove youtubers
+    [C]lear recent video database
+    [Q]uit''')
+        response = input()
+        if response in ['r', 'R']:
+            print('Removing youtubers')
+            name = input()
+            if name in channels:
+                channels.remove(name)
+                ytdb['channels'] = channels
+
+        elif response in ['c', 'C']:
+            recent_videos = []
+
+        elif response in ['q', 'Q']:
+            valid_input = True
+            print('Youtubers saved, goodbye.')
+
+        elif response in ['d', 'D']:
+            refresh_catalogue()
+
+        elif response in ['a', 'A']:
+            channels = new_channels(channels) # Kind of ugly ngl. It appends within the function
+            ytdb['channels'] = channels
+
+        else:
+            valid_input = False
+            print("Please enter a valid key")
+    ytdb['channels'] = channels
+    ytdb['recent'] = recent_videos
+    global videos_downloaded
+    print(str(videos_downloaded) + ' video(s) downloaded today.')
+    global today_watchtime
+    print('Downloaded today: ' + get_watchtime(today_watchtime))
+    watchtime += today_watchtime
+    print('Total watchtime: ' + get_watchtime(watchtime))
+    ytdb['watchtime'] = watchtime
+    print('Video profile saved. Goodbye')    
     exit(0)
+
+
+today_watchtime = 0
+channels = []
+recent_videos = []
+watchtime = 0
+videos_downloaded = 0
+lock = threading.Lock()
+if not os.path.isdir('downloads'):
+    os.mkdir('downloads')
+
+os.chdir('downloads')
+if __name__ == '__main__':
+    main()
