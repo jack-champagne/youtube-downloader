467c143b4f0212c8dcf6a6e0a56239ba6c16e145
Jack Champagne <jackchampagne.r@gmail.com>
2020-12-16 19:50:39 -0500

    Fix redundant page calls

--- src/main.py
+++ src/main.py
@@ -25,15 +25,16 @@ def start():
         '''========= Welcome to Youtube-Downloader ==========
         Here are your stats
         * Total Watchtime = %d minutes
-        * Total Videos = %d videos''' % (my_stats.get('Watchtime'), my_stats.get('Videos'))
+        * Total Videos = %d videos''' % (my_stats.get('Watchtime') / 60.0, my_stats.get('Videos'))
     )
     new_page()
 
     display_menu(my_catalogue)
-
+    new_page()
     
     catalogue.save_catalogue(my_catalogue)
     downloader.save_stats(my_stats)
+
 
 def display_menu(my_cat):
     # Ask for input
@@ -51,41 +52,31 @@ def display_menu(my_cat):
         * [Q]uit''')
 
         response = input()
+        new_page()
         if response in ['r', 'R']:
-            new_page()
             my_cat['Channels'] = catalogue.delete_channel(my_cat['Channels'])
-            new_page()
 
         elif response in ['c', 'C']:
-            new_page()
             my_cat['Downloaded'] = []
             print("Downloaded videos cleared!")
-            new_page()
 
         elif response in ['q', 'Q']:
-            new_page()
             print("Goodbye!")
             running = False
 
         elif response in ['d', 'D']:
-            new_page()
             catalogue.download_new(my_cat)
-            new_page()
 
         elif response in ['a', 'A']:
-            new_page()
             my_cat['Channels'] += catalogue.add_new_channels()
-            new_page()
 
         elif response in ['v', 'V']:
-            new_page()
             print('SUBSCRIBED CHANNELS:\n')
 
             if len(my_cat['Channels']) != 0:
                 print('\n'.join(my_cat['Channels']))
             else:
                 print('No channels found!')
-            new_page()
 
         elif response in ['p', 'P']:
             print('PAUSED')
@@ -94,10 +85,10 @@ def display_menu(my_cat):
             print(downloader.get_stats())
 
         else:
-            new_page()
             print('Invalid Input: ' + response)
-            new_page()
-        
+
+        new_page()
+
 
 def new_page():
     print('==================================================')
